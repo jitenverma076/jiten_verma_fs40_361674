f5288f7634af18516118e7ee209c6880bb3b1e1c
Jiten <jitenverma076@gmail.com>
2025-06-10 22:55:56 +0530

    source code update

--- config.js
+++ config.js
@@ -1,13 +1,22 @@
 // Configuration file for API keys
 const config = {
-    YOUTUBE_API_KEY: 'AIzaSyDEAOo1SpxKOZmZqALp9tAJdbVGjgq-3nA'
+    apiKey: 'AIzaSyDEAOo1SpxKOZmZqALp9tAJdbVGjgq-3nA',
+    defaultParams: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 20
+    },
+    endpoints: {
+        search: 'https://www.googleapis.com/youtube/v3/search',
+        videos: 'https://www.googleapis.com/youtube/v3/videos'
+    }
 };
 
 // Debug log to check if config is loaded
-if (!config.YOUTUBE_API_KEY) {
-    console.warn('Warning: YouTube API key is not set. Please set the YOUTUBE_API_KEY environment variable.');
+if (!config.apiKey) {
+    console.warn('Warning: YouTube API key is not set. Please set the apiKey property in config.js.');
 }
 
 Object.freeze(config);
 
-export default config; 
+export default config;
